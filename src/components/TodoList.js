5bbc4e7840be1235d39144b3d32b1a1775047e2f
Aliaksandr Kassirau <aliaksandr_kassirau@epam.com>
2016-05-31 17:10:47 +0300

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todo from './Todo';
+
+export default function TodoList(props) {
+  const { todos } = props;
+  return (
+    <div className='todo'>
+      <input type='text' placeholder='Add todo' />
+      <ul className='todo__list'>
+        {todos.map(t => (
+          <li key={t.id} className='todo__item'>
+            <Todo todo={t} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
